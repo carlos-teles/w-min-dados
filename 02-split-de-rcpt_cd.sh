3349af27e007449391895045353b2dab00e2eee9
Carlos Teles <carlos.teles@eic.cefet-rj.br>
2017-08-12 23:34:01 -0300

    Add first line to splited files

--- 02-split-de-rcpt_cd.sh
+++ 02-split-de-rcpt_cd.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+line=$(head -n 1 /tmp/rcpt_cd.csv)
 /usr/bin/split -l 1000000 /tmp/rcpt_cd.csv /tmp/rcpt_cd_part
+#Do a for 
+FILES=/tmp/rcpt_cd_part*
+for f in $FILES
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  #cat $f
+  sed -i '1i\'"$line" $f
+done
