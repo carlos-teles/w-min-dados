20380258f67b04293765ff9b33c82d5f2be23a7f
Carlos Teles <carlos.teles@eic.cefet-rj.br>
2017-08-28 20:02:36 -0300

    SEM COMENTARIOS AGORA

--- 01-tratamento_original_de_dados.py
+++ 01-tratamento_original_de_dados.py
@@ -10,22 +10,21 @@ pandas.set_option('display.max_columns', None)
 #
 #	Ultimo Download da base foi feito dia 09/08/2017
 #
-#def download_csv(name):
-#    """
-#    Accepts the name of a calaccess.download CSV and returns its path.
-#    """
-#    path = os.path.join(os.getcwd(), '{}.csv'.format(name))
-#    if not os.path.exists(path):
-#        url = "http://calaccess.download/latest/{}.csv".format(name)
-#        r = requests.get(url, stream=True)
-#        with open(path, 'w') as f:
-#            total_length = int(r.headers.get('content-length'))
-#            for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
-#                if chunk:
-#                    f.write(chunk)
-#                    f.flush()
-#    return path
-#
-#rcpt_path = download_csv("rcpt_cd")
-#ff_path = download_csv("filer_filings_cd")
-sys.exit(0)
+def download_csv(name):
+    """
+    Accepts the name of a calaccess.download CSV and returns its path.
+    """
+    path = os.path.join(os.getcwd(), '{}.csv'.format(name))
+    if not os.path.exists(path):
+        url = "http://calaccess.download/latest/{}.csv".format(name)
+        r = requests.get(url, stream=True)
+        with open(path, 'w') as f:
+            total_length = int(r.headers.get('content-length'))
+            for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+    return path
+
+rcpt_path = download_csv("rcpt_cd")
+ff_path = download_csv("filer_filings_cd")
